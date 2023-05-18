c4dff3f7e8d15bc4e20572f4aa6bc24d0c739140
101Mongfa <130193437+101Mongfa@users.noreply.github.com>
2023-05-18 21:47:38 +0700

    Add files via upload

---
+++ src/components/loaderHome/LoaderHome.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {motion} from "framer-motion";
+
+import "./loaderHome.css";
+
+const LoaderHome = () => {
+  return <section>
+    <div className="containerLoaderHome">
+        <span className="loaderText">HAlo</span>
+        <span className="loaderText">Helo</span>
+        <span className="loaderText">Hola</span>
+        <span className="loaderText">haha</span>
+        <span className="loaderText">hehe</span>
+    </div>
+  </section>
+};
+
+export default LoaderHome;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoaderHome from "../components/loaderHome/LoaderHome";
+
+const Home = () => {
+  return (
+    <div>
+        <LoaderHome />
+    </div>
+  );
+};
+
+export default Home;
